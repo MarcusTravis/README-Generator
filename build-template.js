87ca9e991aaeed2cbc1dace611b9759eb46e28b7
Marcus <mpt789@gmail.com>
2020-05-28 15:26:44 -0700

    init commit after first try, seems to work

---
+++ build-template.js
@@ -0,0 +1,61 @@
+
+module.exports = function (user) {
+    return` 
+# ${user.title}
+
+${user.description}
+
+## Getting Started
+
+${user.gettingStarted + " See deployment for notes on how to deploy the project on a live system."}
+
+
+### Prerequisites
+
+${user.preReqs}
+
+\`\`\` 
+Give examples
+\`\`\`
+
+### Installing
+
+${user.install}
+
+
+\`\`\`
+Give the example
+End with an example of getting some data out of the system or using it for a little demo
+\`\`\`
+
+## Deployment
+
+${user.deploy}
+
+## Built With
+
+${user.buildWith}
+
+## Contributing
+
+${user.contributers}
+
+## Versioning
+(Update this as needed)
+We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/your/project/tags). 
+
+## Authors
+
+${user.authors}
+
+
+## License
+
+${user.license}
+
+## Acknowledgments
+
+${user.acknowledge}
+* Hat tip to anyone whose code was used
+* Inspirations:${" " + user.inspirations}
+`};
--- index.js
+++ index.js
@@ -1,23 +1,81 @@
-const inquirer = require("inquirer");
+//Node Modules
 const fs = require("fs");
+const util = require("util");
+
+//NPM Packages
+const inquirer = require("inquirer");
+
+//Local Modules
+const buildTemplate = require("./build-template");
+
+//Promisify
+const writeFile = util.promisify(fs.writeFile);
+
 
 inquirer.prompt([
     {
-            name: "name",
-            type: "input",
-            message: "What is your name?",
-    }
+        name:"title",
+        type:"input",
+        message:"What is the project title?",
+    },
     {
-            name: "about",
-            type: "input",
-            message: "Tell me about your repo...",
-    }
+        name:"description",
+        type:"input",
+        message:"One Paragraph of project description goes here:",
+    },
+    {
+        name: "gettingStarted",
+        type: "input",
+        message: "Write instructions that will get you a copy of the project up and running on your local machine for development and testing purposes.",
+    },
+    {
+        name: "preReqs",
+        type: "input",
+        message: "What software does the user need to install and how do you install them?",
+    },
+    {
+        name: "install",
+        type: "input",
+        message: "Give me a step by step series of examples that tell you how to get a development env running(use <br> to make line breaks between steps i.e. Step 01. Install<br> Step 02. Next step):",
+    },
+    {
+        name: "deploy",
+        type: "input",
+        message: "Add additional notes about how to deploy this on a live system:",
+    },
+    {
+        name: "buildWith",
+        type: "input",
+        message: "Give frameworks etc.. i.e.:* [Dropwizard](http://www.dropwizard.io/1.0.2/docs/) - The web framework used * [Maven](https://maven.apache.org/) - Dependency Management * [ROME](https://rometools.github.io/rome/) - Used to generate RSS Feeds:",
+    },
+    {
+        name: "contributers",
+        type: "input",
+        message: "Tell me any and all contributers to project:",
+    },
+    {
+        name: "authors",
+        type: "input",
+        message: "Who were the Authors? (please use this format:* **Billie Thompson** - *Initial work* - [PurpleBooth](https://github.com/PurpleBooth)):",
+    },
+    {
+        name: "license",
+        type: "input",
+        message: "Input lisences:",
+    },
+    {
+        name: "acknowledge",
+        type: "input",
+        message: "Acknowledgements:",
+    },
+    {
+        name: "inspirations",
+        type: "input",
+        message: "Inspirations:",
+    },
 ])
-.then(function (user) {
-    const template = `
-    # ${user.name}
+.then(function (user){
+    console.log(user);
 
-    ## ${user.about}`
-    fs.writeFile()
+    writeFile("newREADME.md", buildTemplate(user));
 });
-
